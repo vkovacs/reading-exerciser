83ffdc01a6070c26ce568178fb322e63ae350b8a
Viktor Kovacs <crusader8@gmail.com>
2022-01-28 21:37:14 +0100

    use lamda

--- src/main/groovy/crs/readingexerciser/Application.groovy
+++ src/main/groovy/crs/readingexerciser/Application.groovy
@@ -7,45 +7,42 @@ import javazoom.jl.player.Player
 
 import javax.swing.*
 import java.awt.*
-import java.awt.event.ActionEvent
-import java.awt.event.ActionListener
 
 class Application {
+    static TextService textService = new TextService("src/main/resources/Misi.txt")
+
     static void main(String[] args) {
-        TextService textService = new TextService("src/main/resources/Misi.txt")
+
         def word = textService.randomWord()
 
-        JFrame f = new JFrame("Olvasás Gyakoroltató")
+        JFrame frame = new JFrame("Olvasás Gyakoroltató")
         JLabel jLabel = new JLabel()
         jLabel.setFont(new Font("Serif", Font.PLAIN, 42))
         jLabel.text = word
         jLabel.setBounds(50, 20, 600, 50)
         JButton readButton = new JButton("Olvass")
         readButton.setBounds(50, 90, 95, 30)
-        readButton.addActionListener(new ActionListener() {
-            void actionPerformed(ActionEvent e) {
-                ByteString speech = TextToSpeechService.say(word)
-                def stream = new ByteArrayInputStream(speech.toByteArray())
-                Player playMP3 = new Player(stream)
-                playMP3.play()
-
-            }
+        readButton.addActionListener((e) -> {
+            ByteString speech = TextToSpeechService.say(word)
+            def stream = new ByteArrayInputStream(speech.toByteArray())
+            Player playMP3 = new Player(stream)
+            playMP3.play()
         })
+
         JButton nextButton = new JButton("Következő")
         nextButton.setBounds(200, 90, 95, 30)
-        nextButton.addActionListener(new ActionListener() {
-            void actionPerformed(ActionEvent e) {
-                word = textService.randomWord()
-                jLabel.setText(word)
-            }
+        nextButton.addActionListener((e) -> {
+            word = textService.randomWord()
+            jLabel.setText(word)
         })
-        f.add(readButton)
-        f.add(jLabel)
-        f.add(nextButton)
-        f.setSize(600, 180)
-        f.setLayout(null)
-        f.setLocationRelativeTo(null)
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.setVisible(true)
+
+        frame.add(readButton)
+        frame.add(jLabel)
+        frame.add(nextButton)
+        frame.setSize(600, 180)
+        frame.setLayout(null)
+        frame.setLocationRelativeTo(null)
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true)
     }
 }
