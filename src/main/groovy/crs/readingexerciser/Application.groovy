34ef2ee11e4db554351cc37c9dac9a63c26f857b
Viktor Kovacs <crusader8@gmail.com>
2022-01-28 20:37:50 +0100

    has gui

--- src/main/groovy/crs/readingexerciser/Application.groovy
+++ src/main/groovy/crs/readingexerciser/Application.groovy
@@ -5,19 +5,36 @@ import crs.readingexerciser.text.TextService
 import crs.readingexerciser.text.speech.TextToSpeechService
 import javazoom.jl.player.Player
 
+import javax.swing.*
+import java.awt.event.ActionEvent
+import java.awt.event.ActionListener
+
 class Application {
     static void main(String[] args) {
-
         TextService textService = new TextService("src/main/resources/Peterke.txt")
-
         def word = textService.randomWord()
-        println word
-        System.in.newReader().readLine()
 
-        ByteString speech = TextToSpeechService.say(word)
+        JFrame f = new JFrame("Button Example")
+        final JTextField tf = new JTextField()
+        tf.setBounds(50, 50, 150, 20)
+        JButton b = new JButton("Click Here")
+        b.setBounds(50, 100, 95, 30)
+        b.addActionListener(new ActionListener() {
+            void actionPerformed(ActionEvent e) {
+                ByteString speech = TextToSpeechService.say(word)
+                def stream = new ByteArrayInputStream(speech.toByteArray())
+                Player playMP3 = new Player(stream)
+                playMP3.play()
+                word = textService.randomWord()
+                tf.setText(word)
+            }
+        })
+        f.add(b)
+        f.add(tf)
+        f.setSize(400, 400)
+        f.setLayout(null)
+        f.setVisible(true)
 
-        def stream = new ByteArrayInputStream(speech.toByteArray())
-        Player playMP3 = new Player(stream)
-        playMP3.play();
+        tf.text = word
     }
 }
